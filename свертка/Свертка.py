63f62541d9ef6de5dc5428126c68df27d53b248a
Александр Хасанов < >
2017-10-22 14:51:11 +0300

    convolution

--- свертка/Свертка.py
+++ свертка/Свертка.py
@@ -5,7 +5,7 @@ def newImage(pix, widthImg, heightImg):
     draw = ImageDraw.Draw(newImg)
     for i in range(widthImg):
         for j in range(heightImg):
-            draw.point((i, j), (pix[i][j]))
+            draw.point((i, j), (pix[j][i]))
     return newImg
 
 def ReLU(x):
@@ -16,8 +16,8 @@ def ReLU(x):
 
 def convolution(pix, matrix, widthImg, heightImg, sizeMatrix, padding):
     newPix=[[0]*widthImg for i in range(heightImg)]
-    for i in range(widthImg):
-        for j in range(heightImg):
+    for i in range(heightImg):
+        for j in range(widthImg):
             s=0
             #перемножение матриц
             for w in range(sizeMatrix):
@@ -29,21 +29,25 @@ def convolution(pix, matrix, widthImg, heightImg, sizeMatrix, padding):
 
 def transformPix(pix, width, height, padding):
     newMatrix=[[0]*(width + 2*padding) for i in range(height + 2*padding)]
-    for i in range(height):
-        for j in range (width):
-            newMatrix[i+padding][j+padding]=pix[i, j]
+    #print(newMatrix)
+    for i in range(width):
+        #print(i)
+        for j in range (height):
+            #print(j)
+            #print(pix[i,j])
+            newMatrix[j + padding][i + padding]=pix[i,j]
     return newMatrix
 
 if __name__=='__main__':
-    #matrix=((0, 1, 0), (1, -4, 1), (0, 1,  0))
+    matrix=((0, 1, 0), (1, -4, 1), (0, 1,  0))
     path=input('Введите путь до изображения\n')
     k=int(input('Введите размерность матрицы(2*k+1)\n'))
     sizeMatrix=2*k+1
-    matrix=[[0]*sizeMatrix for i in range(sizeMatrix)]
-    print('Введите матрицу')
-    for i in range(sizeMatrix):
-        for j in range(sizeMatrix):
-            matrix[i][j]=int(input())                
+    #matrix=[[0]*sizeMatrix for i in range(sizeMatrix)]
+    #print('Введите матрицу')
+    #for i in range(sizeMatrix):
+    #    for j in range(sizeMatrix):
+    #        matrix[i][j]=int(input())              
     image=Image.open(path).convert('L')
     image=ImageChops.invert(image)
     padding=int((sizeMatrix-1)/2)#отступ c каждой стороны
